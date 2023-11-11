c2afbf20b797d018479a33aa07fe6ed534292194
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-11-11 22:54:44 +0600

    balanced parentheses done

--- Stack Problems/BalancedParentheses.js
+++ Stack Problems/BalancedParentheses.js
@@ -1,1 +1,24 @@
-console.log("Hello");
+function areParenthesesBalanced(str) {
+  let stack = [];
+
+  let parenthesesOrder = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+
+  for (const char of str) {
+    if (char === "(" || char === "{" || char == "[") {
+      stack.push(char);
+    } else {
+      const lastOpenBracket = stack.pop();
+      if (char !== parenthesesOrder[lastOpenBracket]) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
+
+console.log(areParenthesesBalanced("{[()]}"));
+console.log(areParenthesesBalanced(")]}{[("));
