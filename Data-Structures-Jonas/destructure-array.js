3442f37865ef3c430db159b63f90e22ca9a1579a
TonmoyZohani <tonmoyzohani@gmail.com>
2025-10-06 22:02:10 +0600

    destructure code

--- Data-Structures-Jonas/destructure-array.js
+++ Data-Structures-Jonas/destructure-array.js
@@ -1,5 +1,8 @@
-const names = ["Tonmoy", "Taharim", "Inan", "Silmun", "Riyasat"];
+const names = ["Tonmoy", "Taharim", "Inan", "Silmun", ["Riyasat", "Rudro"]];
 
-const [first, second] = names;
+const [first, second, ...restBros] = names;
+const [father, , , , [son1, son2]] = names;
 
-console.log("First -", first);
+// console.log("First -", first);
+console.log("Rest Bros", restBros);
+console.log("Sons", son1, son2);
