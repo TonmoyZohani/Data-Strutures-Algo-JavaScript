479797dabfaead28bf1bef29c45511b636ee15fa
TonmoyZohani <tonmoyzohani@gmail.com>
2025-11-02 00:05:39 +0600

    map complete

--- JavaScript-Data-Trans-Aggr/index.js
+++ JavaScript-Data-Trans-Aggr/index.js
@@ -9,4 +9,29 @@ map.set(course1, { courserId: 1, courseName: "Programming Hero" });
 map.set(course2, { courserId: 2, courseName: "Data Structures" });
 map.set(course3, { courserId: 3, courseName: "Algorithms" });
 
-console.log(map);
+// map.delete(course1);
+// map.has(course1);
+
+// console.log(map);
+// console.log(map.size);
+
+// map.forEach((value, key) => {
+//   console.log("Value:", value);
+//   console.log("Key:", key);
+// });
+
+// console.log([...map.keys()]);
+// console.log([...map.values()]);
+// console.log([...map.entries()]);
+
+for (let [key, value] of map) { // key, value
+  console.log(key, value);
+}
+
+for (let value of map.values()) {
+  console.log(value);
+}
+
+for (let [key, value] of map.entries()) {
+  console.log(key, value);
+}
