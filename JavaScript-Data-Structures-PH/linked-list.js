a2b8b2753328601e39e87d93dd2585dc49d3c771
TonmoyZohani <tonmoyzohani@gmail.com>
2025-11-07 00:43:56 +0600

    queue added

--- JavaScript-Data-Structures-PH/linked-list.js
+++ JavaScript-Data-Structures-PH/linked-list.js
@@ -20,6 +20,148 @@
 //   temp = temp.next;
 // }
 
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
+
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//     this.tail = null;
+//     this.length = 0;
+//   }
+
+//   append(value) {
+//     const newNode = new Node(value);
+
+//     if (this.head === null) {
+//       this.head = newNode;
+//       this.tail = newNode;
+//     } else {
+//       this.tail.next = newNode;
+//       this.tail = newNode;
+//     }
+
+//     this.length++;
+//   }
+
+//   prepend(value) {
+//     const newNode = new Node(value);
+
+//     if (this.head === null) {
+//       this.head = newNode;
+//       this.tail = newNode;
+//     } else {
+//       newNode.next = this.head;
+//       this.head = newNode;
+//     }
+
+//     this.length++;
+//   }
+
+//   insert(index, value) {
+//     if (index < 0 || index > this.length) {
+//       return false;
+//     }
+
+//     if (index === 0) {
+//       this.prepend(value);
+//       return true;
+//     }
+
+//     if (index === this.length) {
+//       this.append(value);
+//       return true;
+//     }
+
+//     const newNode = new Node(value);
+//     let currentNode = this.head;
+//     let currentIndex = 0;
+
+//     while (currentIndex < index - 1) {
+//       currentNode = currentNode.next;
+//       currentIndex++;
+//     }
+
+//     newNode.next = currentNode.next;
+//     currentNode.next = newNode;
+
+//     this.length++;
+//     return true;
+//   }
+
+//   remove(index) {
+//     // Handle edge cases
+//     if (index < 0 || index >= this.length) {
+//       return null; // or throw new Error("Index out of bounds");
+//     }
+
+//     // If list has only one node
+//     if (this.length === 1) {
+//       const removedNode = this.head;
+//       this.head = null;
+//       this.tail = null;
+//       this.length = 0;
+//       return removedNode;
+//     }
+
+//     // Remove from head
+//     if (index === 0) {
+//       const removedNode = this.head;
+//       this.head = this.head.next;
+//       this.length--;
+
+//       if (this.length === 0) {
+//         this.tail = null;
+//       }
+//       return removedNode;
+//     }
+
+//     // Remove from middle or tail
+//     let currentNode = this.head;
+//     let previousNode = null;
+//     let currentIndex = 0;
+
+//     // Traverse to the node to remove
+//     while (currentIndex < index) {
+//       previousNode = currentNode;
+//       currentNode = currentNode.next;
+//       currentIndex++;
+//     }
+
+//     // Remove the node
+//     previousNode.next = currentNode.next;
+
+//     // If we removed the tail, update tail pointer
+//     if (index === this.length - 1) {
+//       this.tail = previousNode;
+//     }
+
+//     this.length--;
+//     return currentNode;
+//   }
+
+//   print() {
+//     let currentNode = this.head;
+
+//     while (currentNode !== null) {
+//       console.log(currentNode.value);
+//       currentNode = currentNode.next;
+//     }
+//   }
+// }
+
+// const list = new LinkedList();
+// list.append(10); // List: 10
+// list.append(30); // List: 10 -> 30
+// list.insert(1, 20); // List: 10 -> 20 -> 30
+// list.insert(0, 5); // List: 5 -> 10 -> 20 -> 30
+// list.insert(4, 40); // List: 5 -> 10 -> 20 -> 30 -> 40
+// list.print(); // Output: 5, 10, 20, 30, 40
+
 class Node {
   constructor(value) {
     this.value = value;
@@ -27,137 +169,92 @@ class Node {
   }
 }
 
-class LinkedList {
+class Queue {
   constructor() {
-    this.head = null;
-    this.tail = null;
+    this.front = null;  // First element in queue (to be removed)
+    this.rear = null;   // Last element in queue (most recently added)
     this.length = 0;
   }
 
-  append(value) {
+  // Add to the end of the queue (enqueue)
+  enqueue(value) {
     const newNode = new Node(value);
 
-    if (this.head === null) {
-      this.head = newNode;
-      this.tail = newNode;
+    if (this.isEmpty()) {
+      // If queue is empty, new node becomes both front and rear
+      this.front = newNode;
+      this.rear = newNode;
     } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
+      // Add to the end and update rear pointer
+      this.rear.next = newNode;
+      this.rear = newNode;
     }
 
     this.length++;
+    return this;
   }
 
-  prepend(value) {
-    const newNode = new Node(value);
+  // Remove from the front of the queue (dequeue)
+  dequeue() {
+    if (this.isEmpty()) {
+      return null; // or throw new Error("Queue is empty");
+    }
 
-    if (this.head === null) {
-      this.head = newNode;
-      this.tail = newNode;
+    const removedNode = this.front;
+
+    if (this.front === this.rear) {
+      // Only one element in queue
+      this.front = null;
+      this.rear = null;
     } else {
-      newNode.next = this.head;
-      this.head = newNode;
+      // Move front pointer to next node
+      this.front = this.front.next;
     }
-
-    this.length++;
-  }
-
-  insert(index, value) {
-    if (index < 0 || index > this.length) {
-      return false;
-    }
-
-    if (index === 0) {
-      this.prepend(value);
-      return true;
-    }
-
-    if (index === this.length) {
-      this.append(value);
-      return true;
-    }
-
-    const newNode = new Node(value);
-    let currentNode = this.head;
-    let currentIndex = 0;
-
-    while (currentIndex < index - 1) {
-      currentNode = currentNode.next;
-      currentIndex++;
-    }
-
-    newNode.next = currentNode.next;
-    currentNode.next = newNode;
-
-    this.length++;
-    return true;
-  }
-
-  remove(index) {
-    // Handle edge cases
-    if (index < 0 || index >= this.length) {
-      return null; // or throw new Error("Index out of bounds");
-    }
-
-    // If list has only one node
-    if (this.length === 1) {
-      const removedNode = this.head;
-      this.head = null;
-      this.tail = null;
-      this.length = 0;
-      return removedNode;
-    }
-
-    // Remove from head
-    if (index === 0) {
-      const removedNode = this.head;
-      this.head = this.head.next;
-      this.length--;
-
-      if (this.length === 0) {
-        this.tail = null;
-      }
-      return removedNode;
-    }
-
-    // Remove from middle or tail
-    let currentNode = this.head;
-    let previousNode = null;
-    let currentIndex = 0;
-
-    // Traverse to the node to remove
-    while (currentIndex < index) {
-      previousNode = currentNode;
-      currentNode = currentNode.next;
-      currentIndex++;
-    }
-
-    // Remove the node
-    previousNode.next = currentNode.next;
-
-    // If we removed the tail, update tail pointer
-    // if (index === this.length - 1) {
-    //   this.tail = previousNode;
-    // }
 
     this.length--;
-    return currentNode;
+    return removedNode.value;
   }
 
+  // Peek at the front element without removing it
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.front.value;
+  }
+
+  // Check if queue is empty
+  isEmpty() {
+    return this.length === 0;
+  }
+
+  // Get the size of the queue
+  size() {
+    return this.length;
+  }
+
+  // Print the queue (for debugging)
   print() {
-    let currentNode = this.head;
+    if (this.isEmpty()) {
+      console.log("Queue is empty");
+      return;
+    }
+
+    let currentNode = this.front;
+    const elements = [];
 
     while (currentNode !== null) {
-      console.log(currentNode.value);
+      elements.push(currentNode.value);
       currentNode = currentNode.next;
     }
+
+    console.log("Queue:", elements.join(" → "));
+  }
+
+  // Clear the queue
+  clear() {
+    this.front = null;
+    this.rear = null;
+    this.length = 0;
   }
 }
-
-const list = new LinkedList();
-list.append(10); // List: 10
-list.append(30); // List: 10 -> 30
-list.insert(1, 20); // List: 10 -> 20 -> 30
-list.insert(0, 5); // List: 5 -> 10 -> 20 -> 30
-list.insert(4, 40); // List: 5 -> 10 -> 20 -> 30 -> 40
-list.print(); // Output: 5, 10, 20, 30, 40
